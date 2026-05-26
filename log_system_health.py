dfda2ed5d65273d1dfe615ee4453281ba159d36e
NAVNEET SINGH <snavneet36@gmail.com>
2026-05-26 10:32:47 +0530

    log_system_health.py

---
+++ log_system_health.py
@@ -0,0 +1,43 @@
+import psutil
+import datetime
+import shutil
+import time
+
+
+def log_system_health( log_file = "System_health_log.txt"):
+
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    cpu_usage = psutil.cpu_percent(interval=1)
+
+    #disk usage in c drive 
+    
+    
+    total,used,free = shutil.disk_usage("C:/")
+    print(f"TOTAL: , {total//(2**30)}, GB | USED: , {used//(2**30)}, GB | FREE: , {free//(2**30)}, GB")
+
+    total,used,free = shutil.disk_usage("D:/")
+    print(f"TOTAL: , {total//(2**30)}, GB | USED: , {used//(2**30)}, GB | FREE: , {free//(2**30)}, GB")
+
+    total,used,free = shutil.disk_usage("E:/")
+    print(f"TOTAL: , {total//(2**30)}, GB | USED: , {used//(2**30)}, GB | FREE: , {free//(2**30)}, GB") 
+
+    with open(log_file, "a") as f:
+        f.write(f"{timestamp} | CPU Usage: {cpu_usage}%\n")
+        f.write(f"Disk Usage (C:/): Total: {total//(2**30)} GB | Used: {used//(2**30)} GB | Free: {free//(2**30)} GB\n")
+        f.write(f"Disk Usage (D:/): Total: {total//(2**30)} GB | Used: {used//(2**30)} GB | Free: {free//(2**30)} GB\n")
+        f.write(f"Disk Usage (E:/): Total: {total//(2**30)} GB | Used: {used//(2**30)} GB | Free: {free//(2**30)} GB\n")
+
+    print(f"System_health_log written at {timestamp}")
+    time.sleep(60)
+
+log_system_health()  
+             
+
+
+
+
+    
+
+
+     
